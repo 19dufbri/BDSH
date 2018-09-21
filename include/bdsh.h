0e69f37e7117b6f3a8356dc1e937e3a51f3eeb8d
19dufbri <bgd1112@gmail.com>
2018-09-21 13:11:13 +0000

    Added copywrite notice to new files.
    Signed-off-by: 19dufbri <bgd1112@gmail.com>

--- include/bdsh.h
+++ include/bdsh.h
@@ -21,6 +21,21 @@
 #define fileExist(x)	access((x), F_OK) == 0
 #define BUF_SIZE	100
 
+/*  Copyright 2018 Brian Duffy
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 // Colors for output
 #define BOLDC		"\x1b[1m"
 #define GREENC		BOLDC "\x1b[38;5;46m" 
